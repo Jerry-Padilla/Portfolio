40ed729f333d42011a6ac1a821f2f74a20f2bdba
Gerardo R Padilla Jr <gerardopadillareynoso@gmail.com>
2025-04-11 13:01:47 -0700

    Created new Pages

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 My Portfolio / CV
 Uses HTML /JS / CSS
 
+
+## Deployment
+Go to:   https://notjerry.vercel.app/ 
+
+
 Powered by Vercel
 
---
+++ src/nav.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("nav.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar").innerHTML = data;
+    })
+    .catch((error) => {
+      console.error("Error loading navigation:", error);
+    });
+});
